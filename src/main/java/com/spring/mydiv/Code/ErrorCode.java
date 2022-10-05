7bcda169ecd710f4c741d453d1ea9a9e7c265d2f
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-10-05 21:33:34 +0900

    Feat: fix error code to 500
    프론트 코드 작성 용이를 위해 모든 에러 코드의 status = 500으로 수정

--- src/main/java/com/spring/mydiv/Code/ErrorCode.java
+++ src/main/java/com/spring/mydiv/Code/ErrorCode.java
@@ -9,9 +9,9 @@ public enum ErrorCode {
 
     NOT_FOUND(404,"COMMON-ERR-404","PAGE NOT FOUND"),
     INTER_SERVER_ERROR(500,"COMMON-ERR-500","INTER SERVER ERROR"),
-    NO_USER(400, "USER-ERR-400", "There is no corresponding USER."),
-    NO_TRAVEL(400, "TRAVEL-ERR-400","There is no corresponding TRAVEL."),
-    NO_EVENT(400, "EVENT-ERR-400","There is no corresponding EVENT."),
+    NO_USER(500, "USER-ERR-500", "There is no corresponding USER."),
+    NO_TRAVEL(500, "TRAVEL-ERR-500","There is no corresponding TRAVEL."),
+    NO_EVENT(500, "EVENT-ERR-500","There is no corresponding EVENT."),
     NO_PAYER(500, "EVENT-ERR-500","There is no PAYER for the event."),
     WRONG_EMAIL(500,"LOGIN-ERR-500","There is no such email information."),
     WRONG_PASSWORD(500, "LOGIN-ERR-500","Invalid password."),
