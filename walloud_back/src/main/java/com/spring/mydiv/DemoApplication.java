f26df9493d9e9e77ebc911eece6842bcafc240eb
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-08 15:00:17 +0900

    fix: s3 업로드 성공

--- walloud_back/src/main/java/com/spring/mydiv/DemoApplication.java
+++ walloud_back/src/main/java/com/spring/mydiv/DemoApplication.java
@@ -10,13 +10,12 @@ public class DemoApplication {
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.properties,"
 			+ "classpath:aws-s3.yml";
-	// + 'classpath:aws-s3.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-//		new SpringApplicationBuilder(DemoApplication.class)
-//				.properties(APPLICATION_LOCATIONS)
-//				.run(args);
+		// SpringApplication.run(DemoApplication.class, args);
+		new SpringApplicationBuilder(DemoApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 		System.out.println("Start!");
 	}
 }
