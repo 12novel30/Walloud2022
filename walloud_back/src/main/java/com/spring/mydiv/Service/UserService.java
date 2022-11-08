07d34c250d993ac10a96930db9e81b8a1222fac4
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-08 18:12:50 +0900

    feat: 유저 이미지 업로드 및 get 이미지 기능 구현

--- walloud_back/src/main/java/com/spring/mydiv/Service/UserService.java
+++ walloud_back/src/main/java/com/spring/mydiv/Service/UserService.java
@@ -77,6 +77,8 @@ public class UserService {
         return result;
     } //fin
 
+
+
     public UserDto.WithTravel getUserInfoWithTravel(int no){
         User entity = userRepository.findById(Long.valueOf(no))
                 .orElseThrow(()-> new DefaultException(NO_USER));
@@ -112,6 +114,21 @@ public class UserService {
         if (updateRequest.getBank() != null) user.setBank(updateRequest.getBank());
 
         return UserDto.Response.fromEntity(userRepository.save(user));
+    }
+
+    @Transactional
+    public UserDto.ResponseWithImage updateUserImage(int userId, String imageURL){
+        User user = userRepository.findById(Long.valueOf(userId))
+                .orElseThrow(() -> new DefaultException(NO_USER));
+        user.setInfo(imageURL);
+        return UserDto.ResponseWithImage.fromEntity(userRepository.save(user));
+    }
+
+    public String getUserImageURL(int userId){
+        return userRepository.findById(Long.valueOf(userId))
+                .map(UserDto.ResponseWithImage::fromEntity)
+                .orElseThrow(()-> new DefaultException(NO_USER))
+                .getImageurl();
     }
 
     public void deleteUser(int userId){
