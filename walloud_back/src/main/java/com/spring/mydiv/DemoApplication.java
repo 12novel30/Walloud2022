25251cca91bfbb264362ee598edf7357836bd709
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-08 15:20:28 +0900

    feat #155: application properties 경로 추가
    fatal 경고 떠서 DemoApplication 수정함

--- walloud_back/src/main/java/com/spring/mydiv/DemoApplication.java
+++ walloud_back/src/main/java/com/spring/mydiv/DemoApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class DemoApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.properties,"
+//			+ "classpath:application.properties,"
+			+ "classpath:walloud_back/src/main/resources/application.properties"
 			+ "classpath:aws-s3.yml";
 
 	public static void main(String[] args) {
