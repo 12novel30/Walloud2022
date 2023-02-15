051838a9b98e70645ee458255560d92f23712acb
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-15 14:12:52 +0900

    feat #158: application.yml 수정 후 DemoApplication run 함수 수정

--- walloud_back/src/main/java/com/spring/mydiv/DemoApplication.java
+++ walloud_back/src/main/java/com/spring/mydiv/DemoApplication.java
@@ -8,15 +8,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class DemoApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.properties,"
+			+ "classpath:application.yml,"
 			+ "classpath:application-awsS3.yml,"
 			+ "classpath:application-OAuth2.yml";
 
 	public static void main(String[] args) {
-		// SpringApplication.run(DemoApplication.class, args);
-		new SpringApplicationBuilder(DemoApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(DemoApplication.class, args);
+//		new SpringApplicationBuilder(DemoApplication.class)
+//				.properties(APPLICATION_LOCATIONS)
+//				.run(args);
 		System.out.println("Start!");
 	}
 }
