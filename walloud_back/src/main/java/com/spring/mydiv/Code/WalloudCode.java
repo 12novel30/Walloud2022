9bc85330c6ebb19b13cb0b59898340c8f4478687
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-16 20:14:15 +0900

    feat #158: create WalloudCode

---
+++ walloud_back/src/main/java/com/spring/mydiv/Code/WalloudCode.java
@@ -0,0 +1,14 @@
+package com.spring.mydiv.Code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.lang.Nullable;
+
+@AllArgsConstructor
+@Getter
+public enum WalloudCode {
+    MANAGER,
+    OTHERS,
+    NO_EVENTS,
+    ;
+}
