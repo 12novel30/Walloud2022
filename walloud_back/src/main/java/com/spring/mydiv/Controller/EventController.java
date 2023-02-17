06125fee07a23d7a2734cb12f2444d0a18a4b1d4
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-18 08:21:19 +0900

    refactor #158: eventController 코드 길이 맞췄음

--- walloud_back/src/main/java/com/spring/mydiv/Controller/EventController.java
+++ walloud_back/src/main/java/com/spring/mydiv/Controller/EventController.java
@@ -44,8 +44,8 @@ public class EventController {
             // create participant
             participantService.createParticipant(
                     participantService.setParticipantRequest(partiDto, eventResponse));
-            // change person(parti) sumSend etc.
-            personService.updatePersonMoney(
+            // update person(parti) sumSend etc.
+            personService.updatePersonMoney( // TODO check
                     PersonDto.tmp.builder()
                             .personId(partiDto.getPersonId())
                             .eventRole(partiDto.getRole())
@@ -56,7 +56,7 @@ public class EventController {
             );
         }
 
-        // change person role in this Travel
+        // update person role for this Travel
         personService.updatePersonRole(travelId);
         // return created event id
         return eventResponse.getEventId();
@@ -66,53 +66,61 @@ public class EventController {
     public void deleteEvent(@PathVariable("travelId") int travelId,
                             @PathVariable("eventId") int eventId)
     {
-        List<ParticipantDto.Detail> partiList = participantService.getPartiDtoListInEvent(eventId);
-        // change person(parti) sumSend etc.
+        // get participant list in this event
+        List<ParticipantDto.Detail> partiList =
+                participantService.getPartiDtoListInEvent(eventId);
+        // update all person(participant) sumSend etc. in this travel before deleting
         for (ParticipantDto.Detail partiDto : partiList){
             personService.updatePersonMoney(
                     PersonDto.tmp.builder()
                             .personId(partiDto.getPersonId())
                             .eventRole(partiDto.isEventRole())
-                            .eventPrice(Double.valueOf(eventService.getEventPriceById(eventId)))
+                            .eventPrice(Double.valueOf(
+                                    eventService.getEventPriceById(eventId)))
                             .chargedPrice(partiDto.getChargedPrice())
                             .isCreate(false)
                             .build()
             );
         }
-        // change person(parti) role
+        // update person role for this Travel
         personService.updatePersonRole(travelId);
         // delete event
         eventService.deleteEvent(eventId);
     }
+
     @PostMapping("/{travelId}/{eventId}/updateEvent")
     public Long updateEvent(
             @PathVariable("travelId") int travelId, @PathVariable("eventId") int eventId,
             @RequestBody EventDto.Request eventUpdateRequest){
-        // update Event
-        EventDto.Response response = eventService.updateEvent(eventId, eventUpdateRequest);
-        // if payer not in parti_list, then add payer to currPartiDtoList
+        // update event
+        EventDto.Response response =
+                eventService.updateEvent(eventId, eventUpdateRequest);
+        // if new payer not in parti_list, then add payer to currPartiDtoList
         List<ParticipantDto.CreateEvent> currPartiDtoList =
                 eventService.validatePayerInPartiList(eventUpdateRequest);
 
-        // get updateRequests from prevPartiDtoList
+        // get updateRequests from prevPartiDtoList // TODO check
         Map<Long, PersonDto.MoneyUpdate> updateRequests =
                 participantService.setMoneyUpdateRequestMap(
                         participantService.getPartiDtoListInEvent(eventId),
                         eventService.getEventPriceById(eventId),
                         eventUpdateRequest.getPrice());
-
         // for each participant,
         for(ParticipantDto.CreateEvent partiDto : currPartiDtoList){
-            WalloudCode isParticipatedChange = participantService.validateIsParticipated(updateRequests, partiDto);
+            WalloudCode isParticipatedChange =
+                    participantService.validateIsParticipated(updateRequests, partiDto);
             if (isParticipatedChange == STILL_PARTICIPATED){
-                PersonDto.MoneyUpdate currRequest = updateRequests.get(partiDto.getPersonId());
+                PersonDto.MoneyUpdate currRequest =
+                        updateRequests.get(partiDto.getPersonId());
                 currRequest.setCurrEventRole(partiDto.getRole());
                 currRequest.setCurrChargedPrice(partiDto.getSpent());
+                // update participant
                 participantService.updateParticipant(
                         partiDto.getRole(),
                         partiDto.getSpent(),
                         partiDto.getPersonId(),
                         Long.valueOf(eventId));
+                // update person(parti) sumSend etc. (delete before event price)
                 personService.updatePersonMoney(
                         PersonDto.tmp.builder()
                                 .personId(partiDto.getPersonId())
@@ -122,6 +130,7 @@ public class EventController {
                                 .isCreate(false)
                                 .build()
                 );
+                // update person(parti) sumSend etc. (delete after event price)
                 personService.updatePersonMoney(
                         PersonDto.tmp.builder()
                                 .personId(partiDto.getPersonId())
@@ -133,8 +142,10 @@ public class EventController {
                 );
             }
             else if (isParticipatedChange == NEW_PARTICIPANT) {
+                // create new participant in updated event
                 participantService.createParticipant(
                         participantService.setParticipantRequest(partiDto, response));
+                // update person(parti) sumSend etc.
                 personService.updatePersonMoney(
                         PersonDto.tmp.builder()
                                 .personId(partiDto.getPersonId())
@@ -147,18 +158,23 @@ public class EventController {
             }
         }
 
-        for(Long personId : updateRequests.keySet()){ // Deleted participant
+        // deleted participant that is neither STILL_PARTICIPATED nor NEW_PARTICIPANT
+        for(Long personId : updateRequests.keySet()){
             PersonDto.MoneyUpdate currRequest = updateRequests.get(personId);
-            if(currRequest.getCurrChargedPrice().equals(-1.0)){
+            // (not updated in STILL_PARTICIPATED phase)
+            if (currRequest.getCurrChargedPrice().equals(-1.0)) {
+                // update person sumSend etc. before deleting
                 personService.updatePersonMoney(
                         PersonDto.tmp.builder()
                                 .personId(personId)
                                 .eventRole(currRequest.isPrevEventRole())
-                                .eventPrice(Double.valueOf(eventService.getEventPriceById(eventId)))
+                                .eventPrice(Double.valueOf(
+                                        eventService.getEventPriceById(eventId)))
                                 .chargedPrice(currRequest.getPrevChargedPrice())
                                 .isCreate(false)
                                 .build()
                 );
+                // delete participant
                 participantService.deleteParticipant(personId, Long.valueOf(eventId));
             }
         }
