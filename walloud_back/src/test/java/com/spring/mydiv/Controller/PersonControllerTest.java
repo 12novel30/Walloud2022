64465d2759375098608230dc3576ad76b46eacf0
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-22 11:06:21 +0900

    merge/feat #158: merge from feature/image-upload - Test

--- walloud_back/src/test/java/com/spring/mydiv/Controller/PersonControllerTest.java
+++ walloud_back/src/test/java/com/spring/mydiv/Controller/PersonControllerTest.java
@@ -1,33 +1,49 @@
 package com.spring.mydiv.Controller;
-
-import org.junit.jupiter.api.DisplayName;
+import com.spring.mydiv.Dto.PersonDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import com.spring.mydiv.Service.ParticipantService;
+import com.spring.mydiv.Service.PersonService;
+import com.spring.mydiv.Service.TravelService;
+import com.spring.mydiv.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@SpringBootTest
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PersonController.class)
 class PersonControllerTest {
-
-    @Autowired(required=true)
-    private PersonController personController;
-
-//    @Test
-//    @Commit
-//    @DisplayName("회원 초대")
-//    void createPerson2Travel() {
-//        //given
-//        int travelId = 57;
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("user_email", "star4007lg@gm.gist.ac.kr");
-//
-//        //when
-//        personController.createPerson2Travel(travelId, map);
-//
-//        //then
-//        System.out.println("fin");
-//    }
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private TravelService travelService;
+    @MockBean
+    private PersonService personService;
+    @MockBean
+    private ParticipantService participantService;
+    protected MediaType contentType = new MediaType(
+            MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            StandardCharsets.UTF_8);
 }
