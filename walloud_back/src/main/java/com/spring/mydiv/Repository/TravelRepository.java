f9d3d2254d5928ed448ee7eb04d315facb86a129
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-22 11:05:05 +0900

    merge/feat #158: merge from feature/image-upload - Repository

--- walloud_back/src/main/java/com/spring/mydiv/Repository/TravelRepository.java
+++ walloud_back/src/main/java/com/spring/mydiv/Repository/TravelRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.mydiv.Entity.Travel;
 
-/**
- * @author 12nov
- */
 public interface TravelRepository extends JpaRepository<Travel, Long>{
-	Optional<Travel> findByName(String name);
 	Optional<Travel> findById(Long no);
+
 	void deleteById(Long travelId);
+
+
+
+	Optional<Travel> findByName(String name);
 }
