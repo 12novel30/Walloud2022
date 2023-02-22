a8d3e44105f736f278ff4c7a30c4a0cb963e9ae4
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-22 11:27:52 +0900

    merge/feat #158: merge from feature/image-upload - DemoApplication.java

--- walloud_back/src/main/java/com/spring/mydiv/DemoApplication.java
+++ walloud_back/src/main/java/com/spring/mydiv/DemoApplication.java
@@ -6,17 +6,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "classpath:application-awsS3.yml,"
-			+ "classpath:application-OAuth2.yml";
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-//		new SpringApplicationBuilder(DemoApplication.class)
-//				.properties(APPLICATION_LOCATIONS)
-//				.run(args);
 		System.out.println("Start!");
 	}
 }
