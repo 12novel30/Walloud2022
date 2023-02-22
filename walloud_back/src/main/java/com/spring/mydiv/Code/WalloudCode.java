553f8eeb3cd1958339f553872089bcf160402377
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-22 10:55:26 +0900

    merge/feat #158: merge from feature/image-upload - Code

---
+++ walloud_back/src/main/java/com/spring/mydiv/Code/WalloudCode.java
@@ -0,0 +1,16 @@
+package com.spring.mydiv.Code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum WalloudCode {
+    MANAGER,
+    OTHERS,
+    NO_EVENTS,
+    STILL_PARTICIPATED,
+    NEW_PARTICIPANT,
+    NOW_NOT_PARTICIPATED,
+    ;
+}
